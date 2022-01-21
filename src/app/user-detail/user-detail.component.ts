83394d6787ce35f3930cd675d7e77c64871fdf39
sassi <attila.sassi@gmail.com>
2022-01-21 22:50:40 +0100

    kész feladat

--- src/app/user-detail/user-detail.component.ts
+++ src/app/user-detail/user-detail.component.ts
@@ -14,7 +14,8 @@ export class UserDetailComponent implements OnInit {
    * @var user {User} - Input tulajdonság
    * @default új User
    */
-
+  
+  @Input() user: User = new User();
 
   @Output() delUser: EventEmitter<User> = new EventEmitter();
 
@@ -31,6 +32,8 @@ export class UserDetailComponent implements OnInit {
    * @param user {User} - az aktuális felhasználó
    * @returns {void}
    */
-
+  onDelete(user: User): void {
+    this.delUser.emit(user);
+  }
 
 }
